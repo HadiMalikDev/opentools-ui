9fb47df0cc35ddd892eb9c4af847bfe6cc6a1e7f
Muhammad Shaheer <shaheer@uptek.com>
2022-10-25 22:44:18 +0500

    UI updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["**/*.{html,js}"],
   theme: {
     colors: {
       primary : 'rgb(39 50 102)',
@@ -8,8 +8,11 @@ module.exports = {
         100: 'rgba(255,255,255,0.7)',
       },
       gray: {
-        300: 'rgb(229 231 235/1)',
         200: 'rgb(229 231 235/1)',
+        300: 'rgb(229 231 235/1)',
+        400: 'rgb(156 163 175/1)',
+        500: 'rgb(107 114 128/1)',
+        900: 'rgb(17 24 39/1)',
       },
       purple: {
         100: 'rgb(243 232 255/1)',
@@ -20,6 +23,11 @@ module.exports = {
         100: 'rgb(220 252 231/1)',
         400: 'rgb(74 222 128/1)',
         600: 'rgb(22 163 74/1)'
+      },
+      slate: {
+        600: 'rgb(71 85 105/1)',
+        700: 'rgb(51 65 85/1)',
+        900: 'rgb(15 23 42/1)'
       }
     },
     fontFamily: {
