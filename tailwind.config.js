54104ca9138f82dd65a6f5ab48c6ffea1a66c0f5
Muhammad Shaheer <shaheer@uptek.com>
2022-10-27 23:04:28 +0500

    3 version of cards added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+
 module.exports = {
   content: ["**/*.{html,js}"],
   theme: {
     colors: {
+      sky: colors.sky,
+      teal: colors.teal,
+      rose: colors.rose,
       primary : 'rgb(39 50 102)',
       white: {
         100: 'rgba(255,255,255,0.7)',
@@ -15,6 +21,7 @@ module.exports = {
         900: 'rgb(17 24 39/1)',
       },
       purple: {
+        50: 'rgb(250 245 255/1)',
         100: 'rgb(243 232 255/1)',
         500: 'rgb(149 39 229/1)',
         600: 'rgb(125 33 190/1)'
@@ -29,6 +36,15 @@ module.exports = {
         600: 'rgb(71 85 105/1)',
         700: 'rgb(51 65 85/1)',
         900: 'rgb(15 23 42/1)'
+      },
+      yellow: {
+        50: 'rgb(254 252 232/1)',
+        700: 'rgb(161 98 7/1)'
+      },
+      indigo: {
+        50: 'rgb(238 242 255/1)',
+        500: 'rgb(99 102 241/1)',
+        700: 'rgb(67 56 202/1)'
       }
     },
     fontFamily: {
